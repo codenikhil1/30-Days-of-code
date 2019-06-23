7395621e2a58c95d93095ccf32b1097a4f064cca
NIkhil Sonawane <51852902+codenikhil1@users.noreply.github.com>
2019-06-23 17:18:38 +0530

    Create Day B:2D_Arrays.cpp

---
+++ Day B:2D_Arrays.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+
+
+int main()
+{
+    vector<vector<int> > arr(6,vector<int>(6));
+    
+    for (int i = 0; i < 6; i++) {
+        
+
+        for (int j = 0; j < 6; j++) {
+            cin >> arr[i][j];
+        }
+    }
+        int sum=-9*7;
+        for(int i=0;i<6;i++){
+            for(int j=0;j<6;j++){
+                 if (j + 2 < 6 && i + 2 < 6) {
+                int max=arr[i][j]+arr[i][j+1]+arr[i][j+2]+arr[i+1][j+1]+arr[i+2][j]+arr[i+2][j+1]+arr[i+2][j+2];
+                 
+                if(sum<max){
+                    sum=max;
+                }
+                 }
+                 
+            }
+        }
+        cout<<sum;
+
+       
+    
+
+    return 0;
+}
