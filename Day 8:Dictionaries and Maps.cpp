ee64d6d7577d73975187adcb853634fa261e85ba
NIkhil Sonawane <51852902+codenikhil1@users.noreply.github.com>
2019-06-27 17:17:36 +0530

    Create Day 8:Dictionaries and Maps.cpp

---
+++ Day 8:Dictionaries and Maps.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<map>
+using namespace std;
+int main() {
+    int n;
+    cin >>n;
+    map<string, string> phonebook;
+    string name, id;
+    for (int i=0; i<n;i++) {
+      cin>>name >> id;
+      phonebook[name] = id;
+    }
+    while(cin >> name) {
+    if (phonebook.find(name) != phonebook.end()) {
+        cout << name << "=" << phonebook.find(name)->second << endl;
+    } else {
+        cout << "Not found" << endl;
+    }
+}
+
+    return 0;
+}
